fdadd5faf5f2e8e4be14a37f4f7972a8cb9c0c77
Zimeng Chen <eagleofpc@gmail.com>
2016-06-18 22:16:35 -0400

    Use correct id reference

--- app/models/payable.js
+++ app/models/payable.js
@@ -4,8 +4,8 @@ var Schema = mongoose.Schema;
 var PayableSchema = new Schema({
   dateStart: { type: Date, required: true, default: Date.now },
   Contractor: {
-    identifier: { type: String, required: true },
-    name: { type: String, required: true }
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Contractor'
   },
   amount: { type: Number, required: true },
   createdTime: { type: Date, default: Date.now },
